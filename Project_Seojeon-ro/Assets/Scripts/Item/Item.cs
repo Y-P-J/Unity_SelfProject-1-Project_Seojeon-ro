aac53dfc4372e0ec911d094939a364c7e714f7b6
Y-P-J <ypjcw0101@naver.com>
2024-04-02 04:34:29 +0900

    240402 업데이트
    이미지 배치 변경
    설명 txt 추가 및 업데이트
    스크립트 추가
    scriptableojbect 추가

---
+++ Project_Seojeon-ro/Assets/Scripts/Item/Item.cs
@@ -0,0 +1,62 @@
+using ItemStruct;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ItemStruct
+{
+    public enum ITEM_TYPE
+    {
+        WEAPON = 1,
+        WEARABLE = 2,
+        CONSUMABLE = 101,
+        MATERIAL = 201,
+    }
+
+    public enum WEAPON_TYPE
+    {
+        SWORD = 1,
+        AXE = 2,
+        BOW = 101,
+        WAND = 201,
+    }
+
+    public enum WEARABLE_TYPE
+    {
+        HELMET = 1,
+        CLOTH = 101,
+        PANTS = 201,
+        GLOVE = 301,
+        BOOTS = 401,
+        RING = 501,
+    }
+
+    public enum QUALITY_TYPE
+    {
+        COMMON = 1,
+        RARE = 11,
+        EPIC = 21,
+    }
+
+    [System.Serializable]
+    public struct ItemStat
+    {
+        public int hp;
+        public int mp;
+        public int atk;
+        public int def;
+        public float cri;
+        public float eva;
+        public int speed;
+    }
+}
+
+[System.Serializable]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    [TextArea(1, 4)] public string description;
+    public Sprite itemImage;
+    public int IdNumber;
+    public ITEM_TYPE itemType;
+}
